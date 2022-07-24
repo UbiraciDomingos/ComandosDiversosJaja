7bd17c0eb63bb99dd784d87b392ddc3eb5015c2e
Ubiraci Domingos <iposuporte@gmail.com>
2022-07-24 12:55:42 -0300

    Arumando o código

---
+++ src/cemExercicioDeFixação/Exercici001Reatangulo.java
@@ -0,0 +1,18 @@
+package cemExercicioDeFixação;
+
+import java.util.Scanner;
+
+public class Exercici001Reatangulo {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x;
+		x = sc.nextInt();
+
+		System.out.println("O tamanho do Triangulo é: " + x);
+
+		sc.close();
+	}
+}
